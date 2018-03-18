7975d4b98be30fea2833ce791412404a41723e85
Tien Pham <r3dsky02@yahoo.com.vn>
2018-03-18 18:25:52 +0200

    SVG loading with state

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -4,11 +4,15 @@ import GridItem from './GridItem';
 import styles from './Grid.css';
 
 export default class Grid extends React.Component {
+  updateClicked(id) {
+    this.props.updateClicked(id);
+  }
   renderFacts() {
     return this.props.facts.map(item => 
       <GridItem 
         key={item.id}
         fact={item}
+        updateClicked={this.updateClicked.bind(this)}
       />
     );
   }
