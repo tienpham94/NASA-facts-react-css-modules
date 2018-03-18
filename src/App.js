274479d509fabc19bd0f2c16a8d252fbc0da7cde
Tien Pham <r3dsky02@yahoo.com.vn>
2018-03-18 18:20:32 +0200

    Set up state and grid component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import Grid from './Grid/Grid';
 import Rocket from './Rocket/Rocket';
 import Smoke from './Rocket/Smoke';
 
+import facts from './NASA_facts.json';
+
 //styles
 import styles from './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      facts,
+    };
+  }
   render() {
     return (
       <div className={styles.app}>
         <Title />
         <Background />
-        <Grid />
+        <Grid facts={this.state.facts}/>
         <Rocket />
         <Smoke />
       </div>
--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import GridItem from './GridItem';
+
+import styles from './Grid.css';
 
 export default class Grid extends React.Component {
+  renderFacts() {
+    return this.props.facts.map(item => 
+      <GridItem 
+        key={item.id}
+        fact={item}
+      />
+    );
+  }
   render() {
     return (
-      <div>
-        <h1>Grid</h1>
-      </div>
+      <section className={styles.grid}>
+        {this.renderFacts()}
+      </section>
     );
   }
 }
