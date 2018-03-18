fcae76581f74a8001de2834f54eec049aa644255
Tien Pham <r3dsky02@yahoo.com.vn>
2018-03-18 18:00:30 +0200

    Initialize with create-react-app for css modules & clean up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+//styles
+import styles from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.header}>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
